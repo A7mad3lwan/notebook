d881cfd2c76de3a7a236cb05d51b9163d26e9f29
A7mad3lwan <ahmadalwan456@gmail.com>
2024-05-23 06:47:27 +0300

    change gradle version

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ void main() async{
 class Notebook extends StatelessWidget {
   const Notebook({super.key});
 
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
