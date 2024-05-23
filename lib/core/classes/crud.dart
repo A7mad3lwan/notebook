2475c69bd990ecbb88c4473d79f3093c8e319edc
Ahmed Elwan <34627356+A7mad3lwan@users.noreply.github.com>
2024-05-23 22:34:59 +0300

    create crud class

---
+++ lib/core/classes/crud.dart
@@ -0,0 +1,41 @@
+import 'dart:convert';
+import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
+
+class Crud {
+  getRequest(String url) async {
+    try {
+      var response = await http.get(Uri.parse(url));
+      if (response.statusCode == 200) {
+        var responseBody = jsonDecode(response.body);
+        return responseBody;
+      } else {
+        if (kDebugMode) {
+          print('error ${response.statusCode}');
+        }
+      }
+    } on Exception catch (e) {
+      if (kDebugMode) {
+        print('error from catch $e');
+      }
+    }
+  }
+
+  postRequest(String url, Map<String, dynamic> data) async {
+    try {
+      var response = await http.post(Uri.parse(url), body: data);
+      if (response.statusCode == 200) {
+        var responseBody = jsonDecode(response.body);
+        return responseBody;
+      } else {
+        if (kDebugMode) {
+          print('error ${response.body}');
+        }
+      }
+    } on Exception catch (e) {
+      if (kDebugMode) {
+        print('error from catch $e');
+      }
+    }
+  }
+}
