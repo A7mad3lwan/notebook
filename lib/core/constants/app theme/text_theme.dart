d29dc7ec4b9ccd7a3ad8597c70cd62f17eaef098
A7mad3lwan <ahmadalwan456@gmail.com>
2024-05-23 04:50:09 +0300

    add theme data and font styles

---
+++ lib/core/constants/app theme/text_theme.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+
+abstract class Styles {
+  static const font20 = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const font18 = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w600,
+  );
+
+  static const font16 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+  );
+}
---
+++ lib/core/constants/app theme/theme_data.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+ThemeData englishTheme = ThemeData(
+  brightness: Brightness.dark,
+  scaffoldBackgroundColor: const Color(0xff100B20),
+  fontFamily: 'PlayfairDisplay',
+  appBarTheme: const AppBarTheme(
+    centerTitle: true,
+    toolbarHeight: 50,
+    elevation: 0,
+    backgroundColor: Color(0xff100B20),
+  ),
+);
+
+ThemeData arabicTheme = ThemeData(
+  brightness: Brightness.dark,
+  scaffoldBackgroundColor: const Color(0xff100B20),
+  fontFamily: 'Cairo',
+  appBarTheme: const AppBarTheme(
+    centerTitle: true,
+    toolbarHeight: 50,
+    elevation: 0,
+    backgroundColor: Color(0xff100B20),
+  ),
+);
