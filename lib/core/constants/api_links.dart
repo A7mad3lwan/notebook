883c5f40ff37d2010d2926e6d37af3e305935825
Ahmed Elwan <34627356+A7mad3lwan@users.noreply.github.com>
2024-05-25 23:17:37 +0300

    create appServices and api links

---
+++ lib/core/constants/api_links.dart
@@ -0,0 +1,4 @@
+const serverLink = 'http://10.0.2.2/nota';
+
+const loginLink = '$serverLink/auth/login.php';
+const signupLink = '$serverLink/auth/signup.php';
--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:notebook/core/constants/app_services.dart';
 
 import 'core/app routes/get_pages.dart';
 import 'core/constants/app theme/theme_data.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await servicesInit();
   runApp(const Notebook());
 }
 
 class Notebook extends StatelessWidget {
   const Notebook({super.key});
-
 
   @override
   Widget build(BuildContext context) {
